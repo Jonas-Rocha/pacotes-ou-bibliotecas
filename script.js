8dfdfbe06b8a23b9dfa2008120144fb597b36276
Jonas Rocha <jonasrpnstudy@gmail.com>
2025-07-17 19:21:39 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,42 @@
+const now = dayjs();
+
+console.log(now.format("DD/MM/YYYY - HH:mm"));
+
+// Gerenciador de pacote
+
+/*
+É uma ferramenta que facilita a instalação, atualização e
+gerenciamento de bibliotecas e dependências de um projeto.
+
+O gerenciador de pacotes lida com a resolução de dependências,
+garantindo que as bibliotecas necessárias estejam disponíveis e 
+em versões compatíveis.
+*/
+
+/*
+NPM:
+
+O npm (Node Package Manager) é um dos gerenciadores de pacotes
+mais populares para JavaScript.
+
+
+Usado principalmente no ecossistema do Node.js, mas também é
+amplamente utilizado em projetos front-endweb.
+
+O npm permite instalar e gerenciar pacotes JavaScript.
+Também permite executar scripts e bibliotecas instaladas.
+ */
+
+/*
+Dependências e Ambientes
+
+Produção:
+
+inclui as dependências (pacotes/bibliotecas) necessárias para
+a execução do projeto em um ambiente de produção.
+
+Desenvolvimento:
+Inclui as dependências que são necessárias apenas durante o 
+desenvolvimento, mas não são necessárias para a execução
+do aplicativo em produção (por exemplo transpiladores).
+*/
